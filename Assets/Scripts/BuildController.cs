0ef0abe4d37d21c277f5429a7095d6a0d18ee9cf
Jacob McGowan <73966737+TheDevAtlas@users.noreply.github.com>
2025-08-02 10:57:43 -0400

    New Finished Build Belt Code

---
+++ Assets/Scripts/BuildController.cs
@@ -0,0 +1,400 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class BuildController : MonoBehaviour
+{
+    [SerializeField] private GameObject conveyorPrefab;
+    
+    [Header("Build Mode Colors")]
+    [SerializeField] private Color ghostPlaceColor = Color.blue;
+    [SerializeField] private Color deleteHighlightColor = Color.red;
+    [SerializeField] [Range(0f, 1f)] private float ghostTransparency = 0.5f;
+    
+    private bool buildMode = false;
+    private GameObject ghostConveyor;
+    private Camera playerCamera;
+    private float rotationY = 0f;
+    private Vector3 lastPlacedPosition;
+    private bool isDragging = false;
+    private List<GameObject> placedConveyors = new List<GameObject>();
+    private GameObject currentHoveredConveyor;
+    private Dictionary<GameObject, Color[]> originalColors = new Dictionary<GameObject, Color[]>();
+    
+    void Start()
+    {
+        playerCamera = Camera.main;
+        if (playerCamera == null)
+            playerCamera = FindObjectOfType<Camera>();
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            ToggleBuildMode();
+        }
+        
+        if (buildMode)
+        {
+            HandleBuildMode();
+        }
+    }
+    
+    void ToggleBuildMode()
+    {
+        buildMode = !buildMode;
+        
+        if (buildMode)
+        {
+            CreateGhostConveyor();
+        }
+        else
+        {
+            DestroyGhostConveyor();
+            RestoreHoveredConveyorColor();
+            isDragging = false;
+        }
+    }
+    
+    void HandleBuildMode()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        Ray ray = playerCamera.ScreenPointToRay(mousePosition);
+        
+        RaycastHit[] hits = Physics.RaycastAll(ray);
+        System.Array.Sort(hits, (x, y) => x.distance.CompareTo(y.distance));
+        
+        GameObject hoveredBelt = null;
+        Vector3 snapPosition = Vector3.zero;
+        bool foundGround = false;
+        
+        if (hits.Length > 0 && Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("=== Raycast Hits ===");
+            for (int i = 0; i < hits.Length; i++)
+            {
+                Debug.Log($"Hit {i}: {hits[i].collider.gameObject.name} on layer {LayerMask.LayerToName(hits[i].collider.gameObject.layer)}");
+            }
+        }
+        
+        foreach (RaycastHit hit in hits)
+        {
+            GameObject hitConveyor = GetConveyorFromHit(hit.collider.gameObject);
+            if (hitConveyor != null)
+            {
+                hoveredBelt = hitConveyor;
+                break;
+            }
+            
+            if (!foundGround)
+            {
+                snapPosition = SnapToGrid(hit.point);
+                foundGround = true;
+            }
+        }
+        
+        if (hoveredBelt != null)
+        {
+            HandleExistingConveyor(hoveredBelt);
+        }
+        else if (foundGround)
+        {
+            HandleEmptySpace(snapPosition);
+        }
+        
+        HandleRotation();
+        HandleDragging();
+    }
+    
+    void HandleExistingConveyor(GameObject conveyor)
+    {
+        if (ghostConveyor != null)
+        {
+            ghostConveyor.SetActive(false);
+        }
+        
+        SetHoveredConveyor(conveyor);
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            placedConveyors.Remove(conveyor);
+            RestoreConveyorColor(conveyor);
+            Destroy(conveyor);
+            currentHoveredConveyor = null;
+        }
+    }
+    
+    void HandleEmptySpace(Vector3 position)
+    {
+        RestoreHoveredConveyorColor();
+        
+        if (ghostConveyor != null)
+        {
+            ghostConveyor.SetActive(true);
+            ghostConveyor.transform.position = position;
+            ghostConveyor.transform.rotation = Quaternion.Euler(0, rotationY, 0);
+            SetGhostColor(ghostPlaceColor);
+        }
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                StartDragging(position);
+            }
+            else
+            {
+                PlaceConveyor(position);
+            }
+        }
+    }
+    
+    void SetHoveredConveyor(GameObject conveyor)
+    {
+        if (currentHoveredConveyor == conveyor)
+            return;
+        
+        RestoreHoveredConveyorColor();
+        
+        currentHoveredConveyor = conveyor;
+        
+        StoreOriginalColors(conveyor);
+        SetConveyorColor(conveyor, deleteHighlightColor);
+    }
+    
+    void RestoreHoveredConveyorColor()
+    {
+        if (currentHoveredConveyor != null)
+        {
+            RestoreConveyorColor(currentHoveredConveyor);
+            currentHoveredConveyor = null;
+        }
+    }
+    
+    void StoreOriginalColors(GameObject conveyor)
+    {
+        if (originalColors.ContainsKey(conveyor))
+            return;
+        
+        Renderer[] renderers = conveyor.GetComponentsInChildren<Renderer>();
+        List<Color> colors = new List<Color>();
+        
+        foreach (Renderer renderer in renderers)
+        {
+            foreach (Material material in renderer.materials)
+            {
+                colors.Add(material.color);
+            }
+        }
+        
+        originalColors[conveyor] = colors.ToArray();
+    }
+    
+    void RestoreConveyorColor(GameObject conveyor)
+    {
+        if (!originalColors.ContainsKey(conveyor))
+            return;
+        
+        Renderer[] renderers = conveyor.GetComponentsInChildren<Renderer>();
+        Color[] storedColors = originalColors[conveyor];
+        int colorIndex = 0;
+        
+        foreach (Renderer renderer in renderers)
+        {
+            foreach (Material material in renderer.materials)
+            {
+                if (colorIndex < storedColors.Length)
+                {
+                    material.color = storedColors[colorIndex];
+                    colorIndex++;
+                }
+            }
+        }
+        
+        originalColors.Remove(conveyor);
+    }
+    
+    void SetConveyorColor(GameObject conveyor, Color color)
+    {
+        Renderer[] renderers = conveyor.GetComponentsInChildren<Renderer>();
+        foreach (Renderer renderer in renderers)
+        {
+            foreach (Material material in renderer.materials)
+            {
+                material.color = color;
+            }
+        }
+    }
+    
+    void HandleRotation()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            rotationY -= 90f;
+            if (rotationY < 0) rotationY = 270f;
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            rotationY += 90f;
+            if (rotationY >= 360f) rotationY = 0f;
+        }
+    }
+    
+    void HandleDragging()
+    {
+        if (isDragging && Input.GetKey(KeyCode.LeftShift))
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            Ray ray = playerCamera.ScreenPointToRay(mousePosition);
+            
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                Vector3 currentPosition = SnapToGrid(hit.point);
+                DrawLine(lastPlacedPosition, currentPosition);
+            }
+        }
+        
+        if (Input.GetMouseButtonUp(0))
+        {
+            isDragging = false;
+        }
+    }
+    
+    void StartDragging(Vector3 startPosition)
+    {
+        isDragging = true;
+        lastPlacedPosition = startPosition;
+        PlaceConveyor(startPosition);
+    }
+    
+    void DrawLine(Vector3 start, Vector3 end)
+    {
+        Vector3 direction = (end - start).normalized;
+        float distance = Vector3.Distance(start, end);
+        
+        if (Mathf.Abs(direction.x) > Mathf.Abs(direction.z))
+        {
+            direction = new Vector3(Mathf.Sign(direction.x), 0, 0);
+        }
+        else
+        {
+            direction = new Vector3(0, 0, Mathf.Sign(direction.z));
+        }
+        
+        Vector3 currentPos = start + direction;
+        
+        while (Vector3.Distance(start, currentPos) <= distance)
+        {
+            if (GetConveyorAtPosition(currentPos) == null)
+            {
+                PlaceConveyor(currentPos);
+            }
+            currentPos += direction;
+        }
+    }
+    
+    GameObject GetConveyorAtPosition(Vector3 position)
+    {
+        foreach (GameObject conveyor in placedConveyors)
+        {
+            if (conveyor != null && Vector3.Distance(conveyor.transform.position, position) < 0.1f)
+            {
+                return conveyor;
+            }
+        }
+        return null;
+    }
+    
+    void PlaceConveyor(Vector3 position)
+    {
+        GameObject newConveyor = Instantiate(conveyorPrefab, position, Quaternion.Euler(0, rotationY, 0));
+        placedConveyors.Add(newConveyor);
+    }
+    
+    GameObject GetConveyorFromHit(GameObject hitObject)
+    {
+        foreach (GameObject conveyor in placedConveyors)
+        {
+            if (conveyor == null) continue;
+            
+            if (hitObject == conveyor || hitObject.transform.IsChildOf(conveyor.transform))
+            {
+                return conveyor;
+            }
+        }
+        return null;
+    }
+    
+    Vector3 SnapToGrid(Vector3 position)
+    {
+        return new Vector3(
+            Mathf.Round(position.x),
+            position.y,
+            Mathf.Round(position.z)
+        );
+    }
+    
+    void CreateGhostConveyor()
+    {
+        if (conveyorPrefab != null)
+        {
+            ghostConveyor = Instantiate(conveyorPrefab);
+            RemoveGhostComponents();
+            SetGhostColor(ghostPlaceColor);
+        }
+    }
+    
+    void RemoveGhostComponents()
+    {
+        if (ghostConveyor != null)
+        {
+            Collider[] colliders = ghostConveyor.GetComponentsInChildren<Collider>();
+            foreach (Collider col in colliders)
+            {
+                Destroy(col);
+            }
+            
+            MonoBehaviour[] scripts = ghostConveyor.GetComponentsInChildren<MonoBehaviour>();
+            foreach (MonoBehaviour script in scripts)
+            {
+                if (script != null && script.GetType() != typeof(Transform))
+                {
+                    Destroy(script);
+                }
+            }
+        }
+    }
+    
+    void DestroyGhostConveyor()
+    {
+        if (ghostConveyor != null)
+        {
+            Destroy(ghostConveyor);
+            ghostConveyor = null;
+        }
+    }
+    
+    void SetGhostColor(Color color)
+    {
+        if (ghostConveyor != null)
+        {
+            Renderer[] renderers = ghostConveyor.GetComponentsInChildren<Renderer>();
+            foreach (Renderer renderer in renderers)
+            {
+                foreach (Material material in renderer.materials)
+                {
+                    material.SetFloat("_Mode", 2);
+                    material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+                    material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                    material.SetInt("_ZWrite", 0);
+                    material.DisableKeyword("_ALPHATEST_ON");
+                    material.EnableKeyword("_ALPHABLEND_ON");
+                    material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                    material.renderQueue = 3000;
+                    
+                    material.color = new Color(color.r, color.g, color.b, ghostTransparency);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,122 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    public float moveSpeed = 5f;
+    public float moveSmoothing = 10f;
+    
+    [Header("Zoom Settings")]
+    public float zoomSpeed = 2f;
+    public float zoomSmoothing = 8f;
+    
+    [Header("Rotation Settings")]
+    public float rotationSpeed = 100f;
+    public float rotationSmoothing = 12f;
+    
+    private Camera cam;
+    private Vector3 targetPosition;
+    private float targetZoom = -10f;
+    private float targetRotationY = 0f;
+    private bool isRotating = false;
+    
+    void Start()
+    {
+        cam = GetComponentInChildren<Camera>();
+        targetPosition = transform.position;
+        
+        if (cam != null)
+        {
+            cam.transform.localPosition = new Vector3(0, 0, targetZoom);
+        }
+    }
+    
+    void Update()
+    {
+        HandleMovement();
+        HandleZoom();
+        HandleRotation();
+        HandleReset();
+        
+        ApplyMovement();
+        ApplyZoom();
+        ApplyRotation();
+    }
+    
+    void HandleMovement()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        
+        Vector3 moveDirection = new Vector3(horizontal, 0, vertical) * moveSpeed * Time.deltaTime;
+        targetPosition += moveDirection;
+        
+        targetPosition.x = Mathf.Clamp(targetPosition.x, -20f, 20f);
+        targetPosition.z = Mathf.Clamp(targetPosition.z, -20f, 20f);
+    }
+    
+    void HandleZoom()
+    {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        targetZoom += scroll * zoomSpeed;
+        targetZoom = Mathf.Clamp(targetZoom, -20f, -5f);
+    }
+    
+    void HandleRotation()
+    {
+        if (Input.GetMouseButtonDown(2))
+        {
+            isRotating = true;
+        }
+        
+        if (Input.GetMouseButtonUp(2))
+        {
+            isRotating = false;
+            SnapToNearestAngle();
+        }
+        
+        if (isRotating)
+        {
+            float mouseX = Input.GetAxis("Mouse X");
+            targetRotationY += mouseX * rotationSpeed * Time.deltaTime;
+        }
+    }
+    
+    void SnapToNearestAngle()
+    {
+        float snapAngle = Mathf.Round(targetRotationY / 30f) * 30f;
+        targetRotationY = snapAngle;
+    }
+    
+    void HandleReset()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            targetPosition = Vector3.zero;
+            targetZoom = -10f;
+            targetRotationY = 0f;
+        }
+    }
+    
+    void ApplyMovement()
+    {
+        transform.position = Vector3.Lerp(transform.position, targetPosition, moveSmoothing * Time.deltaTime);
+    }
+    
+    void ApplyZoom()
+    {
+        if (cam != null)
+        {
+            Vector3 currentLocalPos = cam.transform.localPosition;
+            currentLocalPos.z = Mathf.Lerp(currentLocalPos.z, targetZoom, zoomSmoothing * Time.deltaTime);
+            cam.transform.localPosition = currentLocalPos;
+        }
+    }
+    
+    void ApplyRotation()
+    {
+        Vector3 currentRotation = transform.eulerAngles;
+        currentRotation.y = Mathf.LerpAngle(currentRotation.y, targetRotationY, rotationSmoothing * Time.deltaTime);
+        transform.rotation = Quaternion.Euler(currentRotation);
+    }
+}
